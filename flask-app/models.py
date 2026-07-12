0556ae5b4d20e9fb554f59ef3478a4de96a5d769
Laxyn <laxyn01@gmail.com>
2026-07-12 23:27:41 +0530

    Update models.py

--- flask-app/models.py
+++ flask-app/models.py
@@ -293,6 +293,10 @@ class Settings(db.Model):
 def init_db(app):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', f'sqlite:///{DATABASE_PATH}')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+    'pool_pre_ping': True,
+    'pool_recycle': 280,
+}
     db.init_app(app)
     with app.app_context():
         db.create_all()
